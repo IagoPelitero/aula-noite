7135cc3b30270d89afa3181045c61691d0902346
IagoPelitero <p.iago.ip@gmail.com>
2024-05-27 21:59:30 -0300

    test: Adicionando o json

--- api/script.js
+++ api/script.js
@@ -1,9 +1,43 @@
 //Iniciar o consumo de API utilizando o método async e await
-async function getCep (){
-    const response = await fetch("https://viacep.com.br/ws/09132675/json/");
-    const retorno = await response.json();
-    console.log(retorno);
-}
+async function getCep() {
+    try {
+        const url = "https://viacep.com.br/ws/09132675/json/";
+        const response = await fetch(url, 
+        //     {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     },
+        //     body: JSON.stringify({
+        //         nome: "Iago",
+        //         idade: 26,
+        //         cidade: "Santo André"
+        //     })
+        // }
+    );
+        const retorno = await response.json();
+        console.log(retorno);
+
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 getCep()
 //Segundo método com fetch .then
+
+function getCep01() {
+    fetch("https://viacep.com.br/ws/09132675/json/", 
+    // {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     }
+    // }
+)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+
+}
+getCep01()
